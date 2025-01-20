e57f7d0885b8bb110c6eae0f45ef99aa52b5be21
Documenter.jl <documenter@juliadocs.github.io>
2025-01-20 03:03:36 +0000

    build based on 27c103b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpectraUtils","category":"page"},{"location":"#SpectraUtils","page":"Home","title":"SpectraUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectraUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpectraUtils]","category":"page"}]
+}
