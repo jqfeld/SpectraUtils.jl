cad4e5495028ef00bb99c14e5433fa0f5ffe938c
Documenter.jl <documenter@juliadocs.github.io>
2025-07-06 09:05:59 +0000

    build based on 390c3f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SpectraUtils","page":"Home","title":"SpectraUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectraUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
