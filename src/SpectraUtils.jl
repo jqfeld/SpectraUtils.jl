c3a57fefc21dad3783bcd9a753c6e48d1b55b62a
Jan Kuhfeld <jan@kuhfeld.net>
2025-07-07 20:31:55 +0900

    fix

--- src/SpectraUtils.jl
+++ src/SpectraUtils.jl
@@ -4,6 +4,8 @@ module SpectraUtils
 export Lorentzian, Gaussian, Voigt, VoigtApprx, lineparam
 include("./lineshapes.jl")
 
+export sigma_doppler, gamma_hard_sphere
+include("./linewidths.jl")
 
 
 end
