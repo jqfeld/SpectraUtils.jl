c6abec55db181c91656abcaedd7404040e2f2e14
Jan Kuhfeld <jan@kuhfeld.net>
2025-07-07 10:18:19 +0900

    add amplitude to `Voigt`

--- src/lineshapes.jl
+++ src/lineshapes.jl
@@ -41,7 +41,8 @@ VoigtApprx(x0, sigma, gamma) = VoigtApprx(1., x0, sigma, gamma)
 
 @inline gaussian(x, σ=0.01) = 1 / sqrt(2π) / σ * exp(-x^2 / 2 / σ^2)
 @inline lorenzian(x, γ) = γ / π / (γ^2 + x^2)
-function (L::VoigtApprx{A,X,T})(x, p=[]) where {A, X, T} 
+function (L::VoigtApprx)(x, p=[])
+  A = lineparam(L.ampl, p)
   μ = lineparam(L.x0, p)
   σ = lineparam(L.sigma, p)
   γ = lineparam(L.gamma, p)
@@ -50,7 +51,7 @@ function (L::VoigtApprx{A,X,T})(x, p=[]) where {A, X, T}
   fL = 2 * γ
   f = (max(0.0, fG^5 + 2.69269 * fG^4 * fL + 2.42843 * fG^3 * fL^2 + 4.47163 * fG^2 * fL^3 + 0.07842 * fG * fL^4 + fL^5))^(1 / 5)
   η = 1.36603 * (fL / f) - 0.47719 * (fL / f)^2 + 0.11116 * (fL / f)^3
-  return lineparam(L.ampl,p)*(η * lorenzian(x - μ, f / 2) + (1 - η) * gaussian(x - μ, f / (2 * sqrt(2 * log(2)))))
+  return A*(η * lorenzian(x - μ, f / 2) + (1 - η) * gaussian(x - μ, f / (2 * sqrt(2 * log(2)))))
 end
 
 
@@ -65,9 +66,10 @@ end
 Voigt(x0, sigma, gamma) = Voigt(1., x0, sigma, gamma)
 
 function (L::Voigt)(x, p=[])
+  A = lineparam(L.ampl, p)
   σ = lineparam(L.sigma, p)
   γ = lineparam(L.gamma, p)
-  return real(
+  return A*real(
     faddeeva( (x + im*γ)/σ/sqrt(2) )
   ) / σ / sqrt(2π)
 end
