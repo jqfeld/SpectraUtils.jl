fd77103a59e185b37d805c7c0365da62bed5fa7c
Jan Kuhfeld <jan@kuhfeld.net>
2025-09-17 15:44:05 +0900

    feat: add chainrules for gaussian, lorentzian, and voigt

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -4,18 +4,28 @@ using ChainRulesCore
 using ChainRulesTestUtils
 using Random
 
-import SpectraUtils: gaussian, lorentzian
+import SpectraUtils: gaussian, lorentzian, voigt
 
 @testset "chainrules" begin
   Random.seed!(1)
 
   @testset "lineshapes" begin
-    test_points = (1.5, 2.5, 10.5, 1.6 + 1.6im, 1.6 - 1.6im, 4.6 + 1.6im)
-    for x in test_points, y in test_points
-      if x isa Real && y isa Real
-        # test_scalar(gaussian, x, y)
-        test_frule(gaussian, x, y)
-        test_rrule(gaussian, x, y)
+    test_points = (0., 0.1, 0.5, 1.0, 1.5, 2.5, 10.0,)
+    test_widths = (0.01, 0.1, 0.5, 1.0, 1.5, 5., 10.0, )
+    for x in test_points, σ in test_widths
+      test_frule(gaussian, x, σ)
+      test_rrule(gaussian, x, σ)
+      test_frule(gaussian, -x, σ)
+      test_rrule(gaussian, -x, σ)
+      test_frule(lorentzian, x, σ)
+      test_rrule(lorentzian, x, σ)
+      test_frule(lorentzian, -x, σ)
+      test_rrule(lorentzian, -x, σ)
+      for γ in test_widths
+        test_frule(voigt, x, σ, γ; rtol=1e-3)
+        test_rrule(voigt, x, σ, γ; rtol=1e-3)
+        test_frule(voigt, -x, σ, γ; rtol=1e-3)
+        test_rrule(voigt, -x, σ, γ; rtol=1e-3)
       end
     end
   end
